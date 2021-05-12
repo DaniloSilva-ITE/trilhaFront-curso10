fcc6070174bc9962df020447dbf505e80a62363e
Danilo Silva <danilo.silva@itenabler.com.br>
2021-05-12 11:33:46 -0300

    Inicio, Cap1, Cap2 e Cap3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+
+import { PhotosModule } from './photos/photos.module';
+import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PhotosModule,
+    AppRoutingModule,
+    ErrorsModule,
+    HomeModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
